1ab92f6781efff6fc3c3c48c0575ce60adcced12
alliane <a.umutoniwa1@alustudent.com>
2024-10-14 12:54:03 +0300

    task 3

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+import sys
 
-number = -98  # Change this value to test different cases
+# Use the first argument as the number, default to 0
+number = int(sys.argv[1]) if len(sys.argv) > 1 else 0
 
 print(f"{number} Battery street")
 
