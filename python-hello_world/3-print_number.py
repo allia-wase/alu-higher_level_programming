0d04fc7bbdb3fc98ead5b5a76c57d6cd55a12c46
alliane <a.umutoniwa1@alustudent.com>
2024-10-19 20:23:34 +0300

    task 3

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-number = 333  # Change this value for testing: try 0 or -98
-print(f"{number} Battery street")
+def print_number(number):
+        print(f"{number} Battery street")
+
+        # Test cases
+        print_number(333)    # Positive number
+        print_number(-98)    # Negative number
+        print_number(0)      # Zero
+
+        # Uncomment the following line to see the ValueError for a string input
+        # print_number("string")  # Wrong type
