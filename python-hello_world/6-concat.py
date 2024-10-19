0d6041cdcd8dad8f97bb2f75b4f82a01572767f1
alliane <a.umutoniwa1@alustudent.com>
2024-10-19 22:06:34 +0300

    task 6

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 str1 = "Welcome to "
-str2_holberton = "Holberton School!"
-str2_battery = "Battery Street!"
-print(str1 + str2_holberton)  # Uncomment to print "Welcome to Holberton School!"
-# print(str1 + str2_battery)  # Uncomment to print "Welcome to Battery Street!"
-
+str2 = "Holberton School!"
+print(str1 + str2)
