64c499fc3abeb52cb08ec16287679546705a0964
alliane <a.umutoniwa1@alustudent.com>
2024-10-19 22:15:18 +0300

    task 7

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"                      # Original word
+word_first_3 = word[0:3]               # First 3 letters
+word_last_2 = word[-2:]                 # Last 2 letters
+middle_word = word[1:-1]                # Middle word without first and last letters
+
+print("First 3 letters:", word_first_3) # Print first 3 letters
+print("Last 2 letters:", word_last_2)   # Print last 2 letters
+print("Middle word:", middle_word)       # Print middle word
