87158547dfb00d6cf0cfcdde20f4e3b68403c555
alliane <a.umutoniwa1@alustudent.com>
2024-10-19 20:07:51 +0300

    task 3

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-import sys
+number = 98  # Replace with your desired integer
 
-# Use the first argument as the number, default to 0
-number = int(sys.argv[1]) if len(sys.argv) > 1 else 0
-
-print(f"{number} Battery street")
-
+print(f"{98} Battery street")
