4fa0de6d6294b710a1c60c01d23ad4131830b8dc
alliane <a.umutoniwa1@alustudent.com>
2024-10-23 10:52:21 +0300

    task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+
+my_string = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-         str = (str[39:66] + ' ' + str[-22:-18] + ' ' + str[:6])
-         print(str)
+         result = (my_string[39:66] + ' ' + my_string[-22:-18] + ' ' + my_string[:6])
+         print(result)
