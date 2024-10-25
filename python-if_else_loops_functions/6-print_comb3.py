f7691bf54ccf5ffaa1142c5c26d8fcd28d74a271
alliane <a.umutoniwa1@alustudent.com>
2024-10-25 19:56:56 +0300

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(10):
+for i in range(1, 10):
         for j in range(i + 1, 10):
                     print("{:02}{:02}".format(i, j), end=", " if not (i == 8 and j == 9) else "")
                     print()  # To add a newline at the end
-
