e4a809bc2fedfd33bdf190d137f5488fff5e7673
alliane <a.umutoniwa1@alustudent.com>
2024-10-25 11:04:31 +0300

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-for letter in range(97, 123):
-        if chr(letter) not in ['q', 'e']:
-                    print("{}".format(chr(letter)), end=""):
+for letter in range(97, 123):  # ASCII values for 'a' to 'z'
+        if chr(letter) not in ['e', 'q']:
+                    print("{}".format(chr(letter)), end="")
+
