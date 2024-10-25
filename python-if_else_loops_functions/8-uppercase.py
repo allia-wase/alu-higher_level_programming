59e6493779f14927a5e2043bed3a2c55de7b2327
alliane <a.umutoniwa1@alustudent.com>
2024-10-25 20:54:59 +0300

    task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-
-def to_uppercase(input_string):
-        """Convert the input string to uppercase."""
-            return input_string.upper()
-
-        if __name__ == "__main__":
-                # Test cases
-                    print(to_uppercase("holberton"))  # Expected: HOLBERTON
-                        print(to_uppercase("Holberton School"))  # Expected: HOLBERTON SCHOOL
-                            print(to_uppercase("Holberton School, 98 battery street"))  # Expected: HOLBERTON SCHOOL, 98 BATTERY STREET
-                                print(to_uppercase(""))  # Expected: (empty string)
-                                    print(to_uppercase("98"))  # Expected: 98
-                                        print(to_uppercase("z"))  # Expected: Z
-
+def uppercase(str):
+    for iterator in str:
+        temp = iterator
+        if ord(temp) >= 97 and ord(temp) <= 122:
+            temp = chr(ord(iterator) - 32)
+            print("{}".format(temp), end='')
+            print()
