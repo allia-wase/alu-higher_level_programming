ad4be0d96dbc35e92b07cad853d7e103bcf9b5eb
alliane <a.umutoniwa1@alustudent.com>
2024-11-04 10:43:47 +0200

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -11,6 +11,7 @@ def safe_print_list(my_list=[], x=0):
     return count
 
 
+
 if __name__ == "__main__":                                                          print(safe_print_list([1, 2, 3, 4], 4))
     print(safe_print_list([1, 2], 2))
     print(safe_print_list([1, 2, 3, 4], 4))
