b270b787823182c7f4c8653e30f3e6a79c003463
alliane <a.umutoniwa1@alustudent.com>
2024-11-24 20:39:08 +0000

    task 0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Script to display the size of the response body
-
-# Check if URL is provided as argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a GET request to the URL and display only the size of the response body in bytes
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}' 
-
+# Send a GET request to the URL and measure the size of the response body
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
